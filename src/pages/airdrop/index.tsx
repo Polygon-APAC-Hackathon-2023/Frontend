4d3b13ac873973890a68a464e88d6bd324d1ed6f
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-05-25 06:10:27 +0800

    feat: done create wallet

--- src/pages/airdrop/index.tsx
+++ src/pages/airdrop/index.tsx
@@ -4,12 +4,76 @@ import { useContractRead, useAccount, useConnect, useNetwork } from "wagmi";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import Hypercert from "../../../public/Hypercert.json";
+import { Polybase } from "@polybase/client";
+import fetch from "node-fetch";
 
 const hypercertAddress = "0x2084200f96AFc5d2e0e59829F875F296d25F49D7";
 
 const connector = new MetaMaskConnector({
   chains: [localhost],
 });
+
+// function to generate a random UUID
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    // eslint-disable-next-line
+    const r = (Math.random() * 16) | 0,
+      // eslint-disable-next-line
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    // eslint-disable-next-line
+    return v.toString(16);
+  });
+}
+
+export async function createWallet(description: string) {
+  const idempotencyKey = uuidv4();
+  const url = "https://api-sandbox.circle.com/v1/wallets";
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization:
+        "Bearer SAND_API_KEY:3d48568ed5c14a5856f1e70b5836e6af:b0478a88f58afe0ae1113a8f26fc9a94",
+    },
+    body: JSON.stringify({
+      idempotencyKey: idempotencyKey,
+      description: description,
+    }),
+  };
+
+  const response = await fetch(url, options);
+
+  const json = await response.json();
+  console.log(json.data.walletId);
+  return [json.data.walletId, idempotencyKey];
+}
+
+export async function createBlockchainAddress(
+  walletID: number,
+  idempotencyKey: string
+) {
+  const url = `https://api-sandbox.circle.com/v1/wallets/${walletID}/addresses`;
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization:
+        "Bearer SAND_API_KEY:3d48568ed5c14a5856f1e70b5836e6af:b0478a88f58afe0ae1113a8f26fc9a94",
+    },
+    body: JSON.stringify({
+      idempotencyKey: idempotencyKey,
+      currency: "USD",
+      chain: "MATIC",
+    }),
+  };
+
+  const response = await fetch(url, options);
+
+  const json = await response.json();
+  return json.data.address;
+}
 
 export default function ProgressBar() {
   const [selectedTokenID, setSelectedTokenID] = useState("");
@@ -18,9 +82,11 @@ export default function ProgressBar() {
   const [grantOwner, setGrantOwner] = useState("");
   const [isFundsDeposited, setIsFundsDeposited] = useState(false);
   const [walletAddress, setWalletAddress] = useState("");
-  const [maticAddress, setMaticAddress] = useState("");
   const [inputError, setInputError] = useState(false);
   const [isVerificationClicked, setIsVerificationClicked] = useState(false);
+  const [description, setDescription] = useState("");
+  const [walletID, setWalletID] = useState(0);
+  const [idempotencyKey, setIdempotencyKey] = useState("");
 
   const { isConnected, address } = useAccount();
   const [isConnect, setIsConnect] = useState(false);
@@ -35,35 +101,39 @@ export default function ProgressBar() {
     setInputError(false); // Reset input error when token ID changes
   };
 
-  const { data: dataOne } = useContractRead({
-    abi: Hypercert.abi,
-    address: hypercertAddress,
-    functionName: "grantEnded(uint256)",
-    args: [selectedTokenID],
-    watch: true,
-    onError: (err: any) => {
-      console.error(err);
-    },
-  });
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
 
-  if (dataOne) {
-    const isGrantEnded = dataOne;
-    console.log(isGrantEnded);
-  }
+  // const { data: dataOne } = useContractRead({
+  //   abi: Hypercert.abi,
+  //   address: hypercertAddress,
+  //   functionName: "grantEnded(uint256)",
+  //   args: [selectedTokenID],
+  //   watch: true,
+  //   onError: (err: any) => {
+  //     console.error(err);
+  //   },
+  // });
 
-  const { data: dataTwo } = useContractRead({
-    abi: Hypercert.abi,
-    address: hypercertAddress,
-    functionName: "grantOwner(uint256)",
-    args: [selectedTokenID],
-    watch: true,
-    onError: (err: any) => {
-      console.error(err);
-    },
-  });
+  // if (dataOne) {
+  //   const isGrantEnded = dataOne;
+  //   console.log(isGrantEnded);
+  // }
 
-  if (dataTwo === address) {
-  }
+  // const { data: dataTwo } = useContractRead({
+  //   abi: Hypercert.abi,
+  //   address: hypercertAddress,
+  //   functionName: "grantOwner(uint256)",
+  //   args: [selectedTokenID],
+  //   watch: true,
+  //   onError: (err: any) => {
+  //     console.error(err);
+  //   },
+  // });
+
+  // if (dataTwo === address) {
+  // }
 
   const handleVerifyGrant = async () => {
     if (selectedTokenID === "") {
@@ -74,19 +144,26 @@ export default function ProgressBar() {
 
     // Perform a check before fetching contract data.
     // If grantEnded is true and grantOwner is the same as the connected wallet address, then grant is verified
-    if (isGrantEnded && grantOwner === address) {
-      setIsGrantVerified(true);
-    }
+    // if (isGrantEnded && grantOwner === address) {
+    //   setIsGrantVerified(true);
+    // }
+
+    setIsGrantVerified(true); // Placeholder
 
     setIsVerificationClicked(true); // Set the state to true after verification
   };
 
-  const handleDepositFunds = () => {
-    // Make API calls to Circle to create a wallet and to create a Matic blockchain address
-    // Set walletAddress and maticAddress with the retrieved data
-    setWalletAddress("exampleWalletAddress"); // Placeholder
-    setMaticAddress("exampleMaticAddress"); // Placeholder
-    setIsFundsDeposited(true);
+  const handleDepositFunds = async (description: string) => {
+    //call createWallet() function
+    const [walletID, idempotencyKey] = await createWallet(description);
+    console.log(walletID, idempotencyKey);
+    setWalletID(walletID);
+    setIdempotencyKey(idempotencyKey);
+
+    //call createBlockchainAddress() function
+    const address = await createBlockchainAddress(walletID, idempotencyKey);
+    console.log(address);
+    setWalletAddress(address);
   };
 
   const handleAirdrop = () => {
@@ -174,19 +251,37 @@ export default function ProgressBar() {
         <p className="ml-2">Deposit Funds</p>
       </div>
       {isGrantVerified && (
-        <div className="flex items-center mb-6">
-          <input
-            type="text"
-            className="px-2 py-1 border border-gray-300 rounded"
-            placeholder="Enter Description (Optional)"
-          />
-          <button
-            className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
-            onClick={handleDepositFunds}
-          >
-            Done
-          </button>
-        </div>
+        <>
+          <div className="flex items-center mb-6">
+            <div className="flex items-center">
+              <input
+                type="text"
+                value={description}
+                onChange={handleDescriptionChange}
+                className="px-2 py-1 border border-gray-300 rounded"
+                placeholder="Enter Description (Optional)"
+              />
+            </div>
+            <button
+              className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={() => handleDepositFunds(description)}
+            >
+              Done
+            </button>
+          </div>
+          {walletID && (
+            <div className="flex items-center mb-6">
+              <p className="mr-2">Wallet ID:</p>
+              <p className="text-blue-500">{walletID}</p>
+            </div>
+          )}
+          {walletAddress && (
+            <div className="flex items-center mb-6">
+              <p className="mr-2">Wallet Address:</p>
+              <p className="text-blue-500">{walletAddress}</p>
+            </div>
+          )}
+        </>
       )}
 
       {/* Section 3: Airdrop */}
