1e6790c2c79776d64cc3af87aa970abd1df6fdc5
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-05-26 13:49:04 +0800

    feat: can read from contract, WIP

--- src/pages/airdrop/index.tsx
+++ src/pages/airdrop/index.tsx
@@ -6,12 +6,50 @@ import { InjectedConnector } from "wagmi/connectors/injected";
 import Hypercert from "../../../public/Hypercert.json";
 import { Polybase } from "@polybase/client";
 import fetch from "node-fetch";
+import * as eth from "@polybase/eth";
 
 const hypercertAddress = "0x2084200f96AFc5d2e0e59829F875F296d25F49D7";
 
 const connector = new MetaMaskConnector({
-  chains: [localhost],
+  chains: [localhost, polygonMumbai],
 });
+
+const db = new Polybase({
+  defaultNamespace:
+    "pk/0xbef7617e6fbf3cf8b4177ca17c6ad2ca37e42a31ec447b24b923112d66fb3e0167bc11d11d306ddd9dee47e680b186bd1d250b079e6a054e79b7baa0ffc16260/ReIgnite",
+});
+const collectionReference = db.collection("City");
+
+// Add signer fn
+db.signer(async (data: string) => {
+  // A permission dialog will be presented to the user
+  const accounts = await eth.requestAccounts();
+
+  // If there is more than one account, you may wish to ask the user which
+  // account they would like to use
+  const account = accounts[0];
+
+  const sig = await eth.sign(data, account);
+
+  return { h: "eth-personal-sign", sig };
+});
+
+async function createRecord(data: string) {
+  const recordData = await collectionReference.create([
+    "new-york",
+    "New York",
+    db.collection("Person").record("johnbmahone"),
+  ]);
+}
+
+// createRecord(data);
+
+async function readRecord() {
+  const data = await db.collection("User").get();
+  console.log(data);
+}
+
+// readRecord();
 
 // function to generate a random UUID
 function uuidv4() {
@@ -75,17 +113,54 @@ export async function createBlockchainAddress(
   return json.data.address;
 }
 
+export async function checkTransferStatus(walletID: number) {
+  const url = `https://api-sandbox.circle.com/v1/transfers?walletId=${walletID}&returnIdentities=false`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      authorization:
+        "Bearer SAND_API_KEY:3d48568ed5c14a5856f1e70b5836e6af:b0478a88f58afe0ae1113a8f26fc9a94",
+    },
+  };
+
+  const response = await fetch(url, options);
+
+  const json = await response.json();
+
+  console.log(json);
+
+  //use polygonscan mumbai to check if the transaction is successful and has a minimum of 30 block confirmations
+  const url2 = `https://api-testnet.polygonscan.com/api?module=transaction&action=gettxreceiptstatus&txhash=${json.data.transactionHash}&apikey=YourApiKeyToken`;
+  const options2 = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
+
+  const response2 = await fetch(url2, options2);
+  const json2 = await response2.json();
+  //if json is undefined, return 0 instead of 1
+  if (json2.result === undefined) {
+    return 0;
+  } else {
+    return json2.data?.result.status;
+  }
+}
+
 export default function ProgressBar() {
   const [selectedTokenID, setSelectedTokenID] = useState("");
   const [isGrantVerified, setIsGrantVerified] = useState(false);
   const [isGrantEnded, setIsGrantEnded] = useState(false);
   const [grantOwner, setGrantOwner] = useState("");
   const [isFundsDeposited, setIsFundsDeposited] = useState(false);
+  const [isWalletAddressCreated, setIsWalletAddressCreated] = useState(false);
   const [walletAddress, setWalletAddress] = useState("");
   const [inputError, setInputError] = useState(false);
   const [isVerificationClicked, setIsVerificationClicked] = useState(false);
   const [description, setDescription] = useState("");
-  const [walletID, setWalletID] = useState(0);
+  const [walletID, setWalletID] = useState(null);
   const [idempotencyKey, setIdempotencyKey] = useState("");
 
   const { isConnected, address } = useAccount();
@@ -105,35 +180,27 @@ export default function ProgressBar() {
     setDescription(e.target.value);
   };
 
-  // const { data: dataOne } = useContractRead({
-  //   abi: Hypercert.abi,
-  //   address: hypercertAddress,
-  //   functionName: "grantEnded(uint256)",
-  //   args: [selectedTokenID],
-  //   watch: true,
-  //   onError: (err: any) => {
-  //     console.error(err);
-  //   },
-  // });
+  const { data: dataOne } = useContractRead({
+    abi: Hypercert.abi,
+    address: hypercertAddress,
+    functionName: "grantEnded(uint256)",
+    args: [selectedTokenID],
+    watch: true,
+    onError: (err: any) => {
+      console.error(err);
+    },
+  });
 
-  // if (dataOne) {
-  //   const isGrantEnded = dataOne;
-  //   console.log(isGrantEnded);
-  // }
-
-  // const { data: dataTwo } = useContractRead({
-  //   abi: Hypercert.abi,
-  //   address: hypercertAddress,
-  //   functionName: "grantOwner(uint256)",
-  //   args: [selectedTokenID],
-  //   watch: true,
-  //   onError: (err: any) => {
-  //     console.error(err);
-  //   },
-  // });
-
-  // if (dataTwo === address) {
-  // }
+  const { data: dataTwo } = useContractRead({
+    abi: Hypercert.abi,
+    address: hypercertAddress,
+    functionName: "grantOwner(uint256)",
+    args: [selectedTokenID],
+    watch: true,
+    onError: (err: any) => {
+      console.error(err);
+    },
+  });
 
   const handleVerifyGrant = async () => {
     if (selectedTokenID === "") {
@@ -142,13 +209,17 @@ export default function ProgressBar() {
       setInputError(false);
     }
 
+    console.log("tokenidis:", selectedTokenID);
+    console.log(dataOne, dataTwo);
+
     // Perform a check before fetching contract data.
     // If grantEnded is true and grantOwner is the same as the connected wallet address, then grant is verified
-    // if (isGrantEnded && grantOwner === address) {
-    //   setIsGrantVerified(true);
-    // }
-
-    setIsGrantVerified(true); // Placeholder
+    if (isGrantEnded && grantOwner === address) {
+      setIsGrantVerified(true);
+    } else {
+      setIsGrantVerified(false);
+      alert("Grant has not ended or you are not the grant owner");
+    }
 
     setIsVerificationClicked(true); // Set the state to true after verification
   };
@@ -164,6 +235,17 @@ export default function ProgressBar() {
     const address = await createBlockchainAddress(walletID, idempotencyKey);
     console.log(address);
     setWalletAddress(address);
+  };
+
+  const handleDepositCheck = async (walletID: number) => {
+    //call checkTransferStatus() function
+    if ((await checkTransferStatus(walletID)) === 1) {
+      setIsFundsDeposited(true);
+    } else {
+      setIsFundsDeposited(false);
+      //also tell the user that no funds have been deposited
+      alert("No funds have been deposited");
+    }
   };
 
   const handleAirdrop = () => {
@@ -245,7 +327,7 @@ export default function ProgressBar() {
       <div className="flex items-center mb-6 mt-10">
         <div
           className={`w-6 h-6 rounded-full ${
-            isFundsDeposited ? "bg-blue-500" : "bg-gray-500"
+            isWalletAddressCreated ? "bg-blue-500" : "bg-gray-500"
           }`}
         ></div>
         <p className="ml-2">Deposit Funds</p>
@@ -266,7 +348,7 @@ export default function ProgressBar() {
               className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
               onClick={() => handleDepositFunds(description)}
             >
-              Done
+              Create Wallet Address
             </button>
           </div>
           {walletID && (
@@ -281,11 +363,19 @@ export default function ProgressBar() {
               <p className="text-blue-500">{walletAddress}</p>
             </div>
           )}
+          {walletAddress && walletID && (
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+              onClick={() => handleDepositCheck(walletID)}
+            >
+              I have deposited USDC on Polygon Mumbai to the above address
+            </button>
+          )}
         </>
       )}
 
       {/* Section 3: Airdrop */}
-      <div className="flex items-center mb-6">
+      <div className="flex items-center mb-10 mt-10">
         <div
           className={`w-6 h-6 rounded-full ${
             isFundsDeposited ? "bg-blue-500" : "bg-gray-500"
