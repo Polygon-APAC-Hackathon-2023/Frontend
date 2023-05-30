3fd95fd6e5ad9418f6d9194dedfb7c37847a543b
V.O.T <makluganteng11@gmail.com>
2023-05-30 22:31:31 +0800

    polygonid implementation

--- src/pages/createGrant/index.tsx
+++ src/pages/createGrant/index.tsx
@@ -11,6 +11,8 @@ import {
   NumberIncrementStepper,
   NumberInputField,
   NumberInputStepper,
+  useDisclosure,
+  Text,
 } from "@chakra-ui/react";
 import { localhost } from "@wagmi/chains";
 import { useState } from "react";
@@ -22,7 +24,18 @@ import {
   useWaitForTransaction,
 } from "wagmi";
 import Hypercert from "../../../public/Hypercert.json";
+import Authenticate from "../../../public/Authenticate.json";
 import { BigNumber } from "ethers";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import QRCode from "react-qr-code";
 
 const CreateGrant = () => {
   const [name, setName] = useState<string>("");
@@ -34,6 +47,9 @@ const CreateGrant = () => {
   const [result, setResult] = useState<string>("");
   const [endGrant, setEndGrant] = useState<number>(0);
   const [projectLink, setProjectLink] = useState<string>("");
+  const [walletAddress, setWalletAddress] = useState<string>("");
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { config } = usePrepareContractWrite({
     address: "0x0cDaa4A6df7b761C9785b399470e947e011E1955",
@@ -46,6 +62,20 @@ const CreateGrant = () => {
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
   });
+
+  const { config: config2 } = usePrepareContractWrite({
+    address: "0xEC324E5ED846F2E7aC94c3381056235CE2DB1343",
+    abi: Authenticate.abi,
+    functionName: "addressToId",
+    args: [walletAddress],
+  });
+
+  const { data: data2, write: write2 } = useContractWrite(config2);
+
+  const { isLoading: isLoading2, isSuccess: isSuccess2 } =
+    useWaitForTransaction({
+      hash: data2?.hash,
+    });
 
   const handleSubmit = async () => {
     //call contract
@@ -73,6 +103,39 @@ const CreateGrant = () => {
   const connector = new MetaMaskConnector({
     chains: [localhost],
   });
+
+  const qrProofRequestJson = {
+    id: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+    typ: "application/iden3comm-plain-json",
+    type: "https://iden3-communication.io/proofs/1.0/contract-invoke-request",
+    thid: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+    body: {
+      reason: "Proof request for create grant",
+      transaction_data: {
+        contract_address: "0xf6A4CF2F83165A35b7BE8A0463622B6cBEf958c1",
+        method_id: "b68967e2",
+        chain_id: 80001,
+        network: "polygon-mumbai",
+      },
+      scope: [
+        {
+          id: 1,
+          circuitId: "credentialAtomicQuerySigV2OnChain",
+          query: {
+            allowedIssuers: ["*"],
+            context:
+              "https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld",
+            credentialSubject: {
+              birthday: {
+                $lt: 20020101,
+              },
+            },
+            type: "KYCAgeCredential",
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <>
@@ -146,14 +209,44 @@ const CreateGrant = () => {
                 onChange={(e) => setProjectLink(e.target.value)}
               />
               <FormHelperText>Link to Project (example: Github)</FormHelperText>
-              <Button
-                mt={4}
-                colorScheme="teal"
-                type="submit"
-                onClick={handleSubmit}
-              >
+              <Button mt={4} colorScheme="teal" type="submit" onClick={onOpen}>
                 Create
               </Button>
+              <Modal
+                blockScrollOnMount={false}
+                isOpen={isOpen}
+                onClose={onClose}
+              >
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Proof of ID</ModalHeader>
+                  <ModalCloseButton />
+                  <Input onChange={(e) => setWalletAddress(e.target.value)} />
+                  <ModalBody>
+                    <Text fontWeight="bold" mb="1rem">
+                      QR
+                    </Text>
+                    <QRCode
+                      level="Q"
+                      style={{ width: 256, marginLeft: 20 }}
+                      value={JSON.stringify(qrProofRequestJson)}
+                    />
+                  </ModalBody>
+
+                  <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>
+                      Close
+                    </Button>
+                    <Button
+                      colorScheme="teal"
+                      type="submit"
+                      onClick={handleSubmit}
+                    >
+                      Submit
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
             </FormControl>
             <div>
               {isLoading && <p>Waiting for transaction to be mined...</p>}
