42a05098a1b28dc0325d32aa5295bb9444b65993
Eason Chai <echai2905@gmail.com>
2023-05-30 21:20:38 +0800

    feat: Add balances

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import React from "react";
+import { fetchHypercertBalance } from "../../graphql/queries";
+import { useAccount } from "wagmi";
 
 export default function Home() {
+  const { address, status } = useAccount();
+
+  React.useEffect(() => {
+    const getHypercertBalance = async (address: string) => {
+      const data = await fetchHypercertBalance(address);
+
+      const balances: { [key: string]: string } = {};
+
+      data.account?.ERC1155balances?.forEach((balance: any) => {
+        const identifier = balance.token.identifier;
+        const valueExact = balance.token.balances[0].valueExact;
+        balances[identifier] = valueExact;
+      });
+
+      console.log(balances);
+    };
+
+    if (status === "connected") {
+      getHypercertBalance(address);
+    }
+  }, [address, status]);
+
   return (
-      <div className="flex justify-center align-middle">
+    <div className="flex justify-center align-middle">
       <ConnectButton />
     </div>
   );
