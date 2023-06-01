8b6f08e646246e2ddec37da58bfdd49d7d8165d0
Eason Chai <echai2905@gmail.com>
2023-06-02 01:43:52 +0800

    feat: Add new query

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -16,6 +16,18 @@ const GET_HYPERCERT_BALANCE = `
   }
 `;
 
+const GET_HOLDERS_OF_TOKENID = `
+query GetHoldersOfTokenID($id:ID!) {
+  erc1155Token(id: $id) {
+    balances {
+      account {
+        id
+      }
+    }
+  }
+}
+`;
+
 export async function fetchHypercertBalance(wallet: string) {
   try {
     const { data } = await client.query({
@@ -28,6 +40,23 @@ export async function fetchHypercertBalance(wallet: string) {
   } catch (error) {
     // Handle any errors that occurred during the request
     console.error("Error fetching Hypercert balance:", error);
+    throw error;
+  }
+}
+
+// Send something like this: "0xf41b72fa91eea7c3b87526ebac58be28dccbb32d/0x6"
+export async function fetchHoldersOfTokenID(id: string) {
+  try {
+    const { data } = await client.query({
+      query: gql(GET_HOLDERS_OF_TOKENID),
+      variables: { id },
+    });
+
+    // Process the retrieved data
+    return data;
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    console.error("Error fetching holders of token ID:", error);
     throw error;
   }
 }
